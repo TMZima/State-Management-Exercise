b66ecf6cdec073adc600a496f1bee8ac536d6656
Trey Zima <trey.m.zima@gmail.com>
2025-01-26 13:12:35 -0700

    Created a button to display a message depending on the status of the game

--- src/ActionButton.jsx
+++ src/ActionButton.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const ActionButton = () => {
-  return;
+const ActionButton = ({ status }) => {
+  let message;
+  if (status === "win") {
+    message = "Restart?";
+  } else if (status === "lose") {
+    message = "Restart?";
+  } else if (status === "draw") {
+    message = "Restart?";
+  } else {
+    message = "Fire!";
+  }
+
+  return <button>{message}</button>;
 };
 
 export default ActionButton;
